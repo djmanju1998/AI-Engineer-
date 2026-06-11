4f65f7ed6abfd85d88a7eb3bb9e807bd72279c8a
djmanju1998 <s9879346@gmail.com>
2026-06-11 16:59:15 +0530

    All Task Updated

--- functions.py
+++ functions.py
@@ -1,4 +1,43 @@
-def greet(name):
-    print("Hello", name)
+# def greet(name, age, city, salary):
+#     print("Hello", name)
+#     print("Age:", age)
+#     print("City:", city)
+#     print("Salary:", salary)
 
-greet("Shivaraj")
+# greet("Shivaraj", 25, "Bangalore", 50000)
+
+
+#operators
+# def opp(a, b):
+#     print("Addition:", a + b)
+#     print("Subtraction:", a - b)
+#     print("Multiplication:", a * b)
+#     if b != 0:
+#         print("Division:", a / b)
+#     else:
+#         print("Division: Cannot divide by zero")
+
+# opp(10, 5)
+
+# Task
+# Product_name = input("Enter product name: ")
+# Price = float(input("Enter price: "))
+# Quantity = int(input("Enter quantity: "))
+# Total_cost = Price * Quantity
+# print("Product Name:", Product_name)
+# print("Price:", Price)
+# print("Quantity:", Quantity)
+# print("Total Cost:", Total_cost)
+# Other Method using function
+# Product_name = input("Enter product name: ")
+# Price = float(input("Enter price: "))
+# Quantity = int(input("Enter quantity: "))
+# def calculate_total_cost(price, quantity):
+#     return price * quantity
+
+# Total_cost = calculate_total_cost(Price, Quantity)
+# print("Product Name:", Product_name)
+# print("Price:", Price)
+# print("Quantity:", Quantity)
+# print("Total Cost:", Total_cost)
+
---
+++ list-tuple.py
@@ -0,0 +1,8 @@
+#List
+# fruits = ["apple", "banana", "orange"]
+# fruits.append("grape")
+# print(fruits)  # Output: ['apple', 'banana', 'orange', 'grape']
+
+#Tuple
+days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+print(days)  # Output: ('Monday', 'Tuesday', 'Wednesday', 'Thursday',
