3d43f5681a589723b66a20a7e824b1aa2aee1c5e
djmanju1998 <s9879346@gmail.com>
2026-06-14 17:17:25 +0530

    two files updated

---
+++ loops.py
@@ -0,0 +1,42 @@
+# for loop Exmaple 1
+# employees = ["Alice", "Bob", "Charlie", "David", "Eve"]
+# for employee in employees:
+#     print(employee)
+
+# Example 2
+# for i in range(5,11):
+#     print(i)
+
+# While loop Example 1
+# count = 0
+# while count < 5:
+#     print("Count:", count)
+#     count += 1
+
+# While loop Example 2
+# sales = [100, 200, 300, 400, 500]
+# total = 0
+# for sale in sales:
+#     total += sale
+
+# print("Total Sales:", total)
+
+# Task1
+# for i in range(1, 11):
+#     print(i)
+
+# Task2
+# num = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(num,"x",i,"=", num*i)
+
+# Take 5 numbers from user and print their sum
+# total = 0
+# for i in range(5):
+#     num = int(input("Enter a number: "))
+#     total += num
+# print("Sum:", total)
+
+# Infinite loop example
+# while True:
+#     print("This is an infinite loop. Press Ctrl+C to stop.")
